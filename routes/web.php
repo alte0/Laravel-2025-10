b58c2c560ade3fc9ecf5f39e42458abd5313c3ea
Максим Дмитриев <makc-dmv@yandex.ru>
2025-11-08 11:32:47 +0500

    hw2

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/pages/main');
+});
+
+Route::get('/profile', function () {
+    return view('/pages/profile');
+});
+
+Route::get('/register', function () {
+    return view('/pages/register');
+});
+
+Route::get('/static', function () {
+    return view('/pages/static');
 });
