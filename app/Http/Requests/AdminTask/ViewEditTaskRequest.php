74d4c4779109499ebb340fe44a0f8e27897973b6
Максим Дмитриев <makc-dmv@yandex.ru>
2025-11-30 20:56:25 +0500

    hw6

---
+++ app/Http/Requests/AdminTask/ViewEditTaskRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\AdminTask;
+
+use App\Models\Task;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+
+class ViewEditTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Gate::allows('update', Task::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
